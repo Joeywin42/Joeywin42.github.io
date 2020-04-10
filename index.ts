b5f3b3265644f1501fafcce389ffa2c1ea55bb5f
Joeywin42 <37713419+Hamtaro42@users.noreply.github.com>
2020-04-10 19:36:28 -0400

    revamped again

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-
-
-function button1Function(){
-    let randText = document.getElementById("buttonPlace");
-    randText.innerHTML += "Phawahaha there is words now";
+function button1Function() {
+  let randText = document.getElementById("buttonPlace");
+  randText.innerHTML += "Phawahaha there is words now";
 }
---
+++ secret/secret.ts
@@ -0,0 +1,5 @@
+function getDrink() {
+  let getdrink = document.getElementById("drinks");
+  getdrink.innerHTML +=
+    '<img src="../Pictures/drink.jpg" width="150" length="400" />';
+}
