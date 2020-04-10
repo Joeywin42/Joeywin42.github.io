4de9277547f2cc312ec68e575030bd33d02e681d
Joeywin42 <37713419+Hamtaro42@users.noreply.github.com>
2020-04-10 19:23:27 -0400

    completely revamped and this might be finale

---
+++ webham/index.ts
@@ -0,0 +1,6 @@
+
+
+function button1Function(){
+    let randText = document.getElementById("buttonPlace");
+    randText.innerHTML += "Phawahaha there is words now";
+}
---
+++ webham/secret/secret.ts
@@ -0,0 +1,7 @@
+
+function getDrink(){
+
+
+    let getdrink = document.getElementById("drinks");
+    getdrink.innerHTML += '<img src="../Pictures/drink.jpg" width="150" length="400" />';
+}
