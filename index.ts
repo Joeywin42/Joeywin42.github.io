3695b71bafbb0df3dfb0b0760e85bee212bc150d
Joeywin42 <37713419+Hamtaro42@users.noreply.github.com>
2020-04-11 11:29:55 -0400

    changed index button to append fashion

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 function button1Function() {
-  let randText = document.getElementById("buttonPlace");
-  randText.innerHTML += "Phawahaha there is words now";
+  /*let randText = document.getElementById("buttonPlace");
+  randText.innerHTML += element;
+  */
+ //append child way
+    let newDiv = document.getElementById("buttonPlace");
+    let newContent = document.createTextNode("Phawahaha there is text now!");
+    newDiv.appendChild(newContent);
+
 }
